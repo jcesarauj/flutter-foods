963895c4e1804c06ba755603cc3027af28e7a959
Julio Cesar de Araujo <34680147+jcesarauj@users.noreply.github.com>
2020-08-03 18:07:58 -0300

    include project in gt
    inclusão do projeto

---
+++ android/app/src/main/kotlin/com/example/flutterMeats/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutterMeats
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screen/category-screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutterMeats/data/damy_data.dart';
+import '../components/category-item.dart';
+import '../models/category.dart';
+
+class CategoryScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Vamos cozinhar"),
+      ),
+        body: GridView(
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
+      ),
+      children: DUMMY_CATEGORIES.map((cat) {
+        return CategoryItem(cat);
+      }).toList(),
+    ));
+  }
+}
