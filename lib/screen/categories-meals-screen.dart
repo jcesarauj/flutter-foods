35074f7f8b3c0f516ee8724c73eae4349ea682d3
Julio Cesar de Araujo <34680147+jcesarauj@users.noreply.github.com>
2020-08-06 19:34:10 -0300

    Inclusão da tela de detalhes da comida
    Inclusão da tela de detalhes da comida

--- lib/screen/categories-meals-screen.dart
+++ lib/screen/categories-meals-screen.dart
@@ -1,18 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutterMeats/components/meals-item.dart';
+import 'package:flutterMeats/data/damy_data.dart';
 import 'package:flutterMeats/models/category.dart';
 
 class CategorieMealsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final category = ModalRoute.of(context).settings.arguments as Category;
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(category.id);
+    }).toList();
 
     return Scaffold(
       appBar: AppBar(
         title: Text(category.title),
       ),
-      body: Center(
-        child: Text("Receitas por categorias"),
-      ),
+      body: ListView.builder(
+          itemCount: categoryMeals.length,
+          itemBuilder: (ctx, index) {
+            return MealsItem(categoryMeals[index]);
+          }),
     );
   }
 }
